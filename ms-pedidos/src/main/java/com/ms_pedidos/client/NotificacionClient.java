3710654327a8a7838406be5018015d66dab1e02d
Andres Cereño <andresaacr21@gmail.com>
2026-05-15 18:10:08 -0400

    Configuracion Eureka más FEign

--- ms-pedidos/src/main/java/com/ms_pedidos/client/NotificacionClient.java
+++ ms-pedidos/src/main/java/com/ms_pedidos/client/NotificacionClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.ms_pedidos.client.dto.CrearNotificacionDTO;
 
-@FeignClient(name = "ms-notificaciones", url = "")
+@FeignClient(name = "ms-notificaciones", url = "http://localhost:8090/api/notificacion")
 public interface NotificacionClient {
 
     @PostMapping("/api/notificaciones")
