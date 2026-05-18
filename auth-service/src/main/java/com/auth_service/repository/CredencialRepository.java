977cc163d6e4cf6bdf6bc6e78a55091457b21a87
Andres Cereño <andresaacr21@gmail.com>
2026-05-18 10:48:01 -0400

    error de typeo en repository

--- auth-service/src/main/java/com/auth_service/repository/CredencialRepository.java
+++ auth-service/src/main/java/com/auth_service/repository/CredencialRepository.java
@@ -9,6 +9,6 @@ import com.auth_service.model.Credencial;
 public interface CredencialRepository extends JpaRepository<Credencial, Long> {
     Optional<Credencial> findByEmail(String email);
     Optional<Credencial> findByIdUsuario(Long idUsuario);
-    boolean existsByEmail(String email);
+    boolean existsByEmail(String emailLogin);
     boolean existsByIdUsuario(Long idUsuario);
 }
